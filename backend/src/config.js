eb62befc59c0ecdeee24f379a50be44c9d010d4f
Nikalitan <83236083+Nikalitan@users.noreply.github.com>
2022-09-16 14:12:14 +0300

    Add files via upload

---
+++ backend/src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    db: 'mongodb://c63688_apkcarter_bot:LeLhoWinquqec83@mongo1.c63688.h2,mongo2.c63688.h2,mongo3.c63688.h2/c63688_apkcarter_bot?replicaSet=MongoReplica', /* MongoDB connection link */
+    secretKey: '6LImBbfVAoQe4ENZsUjG', /* VK Mini Apps service secret key */
+    user_id: 378522526, /* VK user id */
+    token: 'vk1.a.S0ZJ-jiFmdCzmRt6yLp5MB3zNuNtAU8dwO0VN4jK_NP6zY15G02CCJHm4093ImsDMh7M3OJkZIkbNy8PO7KOcUxrezCbHviB84jIRevCzRiWxSwrVO4WR_KgV4WTqDp6C0h8hhYnKKwSJIKH84Z8QmDtOvnRkaXAt4Pnkaza83_p5MLjBnU-YhKmyrBWDNZS', /* VK access token */
+    key: 'fRhUIlsA7lku6&93LaK[oHcFEuwYSD;yt1dfoR&bV#YipBKNww' // VK Coin merchant key
+
+};
